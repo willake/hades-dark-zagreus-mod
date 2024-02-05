8d22ac840c4100c383bc9e9d5809c7543b0c1521
Hui En <huienlin.game@gmail.com>
2024-02-05 13:10:10 +0100

    update: successfully modified the data without corruption

--- harpy.lua
+++ harpy.lua
@@ -1,2 +1,3 @@
-UnitSetData.Enemies.Harpy.WeaponOptions = { "EnemySwordWeapon" }
+UnitSetData.Enemies.Harpy.WeaponOptions = { "EnemySwordWeapon", "EnemySwordWeapon2", "EnemySwordWeapon3",
+    "EnemySwordWeaponDash", "EnemySwordParry" }
 UnitSetData.Enemies.Harpy.DisarmedWeapon = "EnemySwordWeapon"
