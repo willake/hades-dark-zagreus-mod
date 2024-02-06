fbccd2601c0c8b5b83048d2d308d2db28a028999
Hui En <huienlin.game@gmail.com>
2024-02-06 23:42:30 +0100

    update: add lines for setting bow weapons for boss

--- harpy.lua
+++ harpy.lua
@@ -1,3 +1,6 @@
-UnitSetData.Enemies.Harpy.WeaponOptions = { "EnemySwordWeapon", "EnemySwordWeapon2", "EnemySwordWeapon3",
-    "EnemySwordWeaponDash", "EnemySwordParry" }
-UnitSetData.Enemies.Harpy.DisarmedWeapon = "EnemySwordWeapon"
+-- UnitSetData.Enemies.Harpy.WeaponOptions = { "EnemySwordWeapon", "EnemySwordWeapon2", "EnemySwordWeapon3",
+--     "EnemySwordWeaponDash", "EnemySwordParry" }
+-- UnitSetData.Enemies.Harpy.DisarmedWeapon = "EnemySwordWeapon"
+
+UnitSetData.Enemies.Harpy.WeaponOptions = { "EnemyBowWeapon" }
+UnitSetData.Enemies.Harpy.DisarmedWeapon = "EnemyBowWeapon"
