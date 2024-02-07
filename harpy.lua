af604758202ed433fccb0c429af25c91efac061b
Hui En <huienlin.game@gmail.com>
2024-02-07 15:46:02 +0100

    update: apply all sword settings

--- harpy.lua
+++ harpy.lua
@@ -2,5 +2,6 @@
 --     "EnemySwordWeaponDash", "EnemySwordParry" }
 -- UnitSetData.Enemies.Harpy.DisarmedWeapon = "EnemySwordWeapon"
 
-UnitSetData.Enemies.Harpy.WeaponOptions = { "EnemySwordWeapon" }
+UnitSetData.Enemies.Harpy.WeaponOptions = { "EnemySwordWeapon", "EnemySwordWeapon2", "EnemySwordWeapon3",
+    "EnemySwordDash", "EnemySwordParry" }
 UnitSetData.Enemies.Harpy.DisarmedWeapon = "EnemySwordWeapon"
