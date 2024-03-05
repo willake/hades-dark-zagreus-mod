d8b91ba9edf1bf4343d3583206827f2d702bb4de
Hui <huienlin.game@gmail.com>
2024-03-05 13:00:36 +0100

    update: force the second room to be Hades room

---
+++ HadesReplacement.lua
@@ -0,0 +1,2 @@
+-- set phases to only 1
+UnitSetData.Enemies.Hades.Phases = 1
--- main.lua
+++ main.lua
@@ -1,1 +1,23 @@
-if not TestMod.Config.Enabled then return end
+if not EvilZagreus.Config.Enabled then return end
+
+DebugPrintf({ Text = "Setup Events" })
+
+OnAnyLoad { "RoomOpening", function(triggerArgs)
+    DebugPrintf({ Text = "Enter RoomOpening, Force next room to Hades boss room" })
+    -- do stuff here
+    ForceNextRoom = "D_Boss01"
+
+    -- Stomp any rooms already assigned to doors
+    for doorId, door in pairs(OfferedExitDoors) do
+        local room = door.Room
+        if room ~= nil then
+            if ForceNextRoom ~= nil then
+                DebugPrint({ Text = "ForceNextRoom = " .. tostring(ForceNextRoom) })
+            end
+
+            local forcedRoomData = RoomData[ForceNextRoom]
+            local forcedRoom = CreateRoom(forcedRoomData)
+            AssignRoomToExitDoor(door, forcedRoom)
+        end
+    end
+end }
