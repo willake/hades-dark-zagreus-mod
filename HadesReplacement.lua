8c031ec3cec358fc24dd589e3cbb4823554c05d6
Hui <huienlin.game@gmail.com>
2024-03-05 16:07:27 +0100

    update: decrease Hades stages to only 1

--- HadesReplacement.lua
+++ HadesReplacement.lua
@@ -1,2 +1,17 @@
 -- set phases to only 1
 UnitSetData.Enemies.Hades.Phases = 1
+UnitSetData.Enemies.Hades.ShrineDataOverwrites.Phases = 1
+
+-- remove all stages, leaving only 1
+UnitSetData.Enemies.Hades.AIStages = 
+{
+    {
+        RandomAIFunctionNames = { "AttackerAI" },
+        SelectPactLevelAIStage = "PactDataStage0",
+        AIData =
+        {
+            AIEndHealthThreshold = 0.0,
+        },
+        StageTransitionGlobalVoiceLines = "FatherSonArgumentVoiceLines",
+    }
+}
