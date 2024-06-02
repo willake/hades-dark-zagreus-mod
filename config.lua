2b374fe1a20efc39b97fb9b4284340e6b87e8f6b
Hui <huienlin.game@gmail.com>
2024-06-02 17:12:16 +0200

    update: looks good

--- config.lua
+++ config.lua
@@ -7,8 +7,8 @@ DarkZagreus.Version = "beta1.3"
 DarkZagreus.DataVersion = "v6" -- for training data, when the data structure chages, the version increases
 
 DarkZagreus.DefaultAIWeapon = {
-    WeaponName = "BowWeapon",
-    ItemIndex = 2
+    WeaponName = "SwordWeapon",
+    ItemIndex = 1
 }
 
 DarkZagreus.DefaultAIActionData = { -- these are the action probabilities
